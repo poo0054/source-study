4d29c9a7b4fba623b451078005f8b0484a3128cd
poo0054 <poo0054@outlook.com>
2023-06-25 17:01:45 +0800

    添加batch

---
+++ spring-batch/src/main/java/org/github/poo0054/BatchApplication.java
@@ -0,0 +1,15 @@
+package org.github.poo0054;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author poo00
+ */
+@SpringBootApplication
+public class BatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BatchApplication.class, args);
+    }
+}
---
+++ spring-batch/src/main/java/org/github/poo0054/config/BatchConfig.java
@@ -0,0 +1,14 @@
+package org.github.poo0054.config;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author zhangzhi
+ */
+@Configuration
+@EnableBatchProcessing
+public class BatchConfig {
+
+
+}
