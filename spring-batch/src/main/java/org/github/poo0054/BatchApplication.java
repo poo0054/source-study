cc1b7c19fe8860fd14a8893107c5c630efbd7fdd
poo0054 <poo0054@outlook.com>
2023-06-27 17:24:05 +0800

    优化

--- spring-batch/src/main/java/org/github/poo0054/BatchApplication.java
+++ spring-batch/src/main/java/org/github/poo0054/BatchApplication.java
@@ -2,12 +2,11 @@ package org.github.poo0054;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 /**
  * @author poo00
  */
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@SpringBootApplication
 public class BatchApplication {
 
     public static void main(String[] args) {
