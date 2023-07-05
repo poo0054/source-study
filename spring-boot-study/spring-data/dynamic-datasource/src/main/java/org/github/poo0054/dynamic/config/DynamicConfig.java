a5de3c811cb25edcc79e3e3217ca02824c034ce3
poo0054 <poo0054@outlook.com>
2023-07-05 14:52:45 +0800

    add dynamic-datasource 事务

--- spring-boot-study/spring-data/dynamic-datasource/src/main/java/org/github/poo0054/dynamic/config/DynamicConfig.java
+++ spring-boot-study/spring-data/dynamic-datasource/src/main/java/org/github/poo0054/dynamic/config/DynamicConfig.java
@@ -1,7 +1,17 @@
 package org.github.poo0054.dynamic.config;
 
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.transaction.TransactionManagerCustomizers;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.jdbc.support.JdbcTransactionManager;
+import org.springframework.transaction.TransactionManager;
+
+import javax.sql.DataSource;
 
 /**
  * @author zhangzhi
@@ -10,4 +20,17 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 @EnableAspectJAutoProxy(exposeProxy = true)
 public class DynamicConfig {
 
+    @Bean
+    @ConditionalOnMissingBean(TransactionManager.class)
+    DataSourceTransactionManager transactionManager(Environment environment, DataSource dataSource,
+                                                    ObjectProvider<TransactionManagerCustomizers> transactionManagerCustomizers) {
+        DataSourceTransactionManager transactionManager = createTransactionManager(environment, dataSource);
+        transactionManagerCustomizers.ifAvailable((customizers) -> customizers.customize(transactionManager));
+        return transactionManager;
+    }
+
+    private DataSourceTransactionManager createTransactionManager(Environment environment, DataSource dataSource) {
+        return environment.getProperty("spring.dao.exceptiontranslation.enabled", Boolean.class, Boolean.TRUE)
+                ? new JdbcTransactionManager(dataSource) : new DataSourceTransactionManager(dataSource);
+    }
 }
